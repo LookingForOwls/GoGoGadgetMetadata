0b65d8763b7e0dc66a49d9f51cdfdddd19564d97
LookingForOwls <lfo@shuffledao.com>
2023-03-14 11:50:38 -0400

    persist web3 connection

--- api.go
+++ api.go
@@ -15,32 +15,39 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
+type web3 struct {
+	client *ethclient.Client
+}
+
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Metadata API!\n")
 }
 
-func Metadata(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// Setup geth client
-	client, err := ethclient.Dial(EnvConfigs.InfuraRPC)
+func NewWeb3(rpc string) *web3 {
+	c, err := ethclient.Dial(rpc)
 	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+		return nil
 	}
+	return &web3{
+		client: c,
+	}
+}
+
+func (c *web3) Metadata(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	// Convert tokenId in URL to Int64
 	token, _ := strconv.ParseInt(ps.ByName("tokenId"), 0, 64)
 	// Check if token minted
-	if !Minted(client, token) {
+	if !Minted(c.client, token) {
 		fmt.Fprintf(w, "Token %s Not Minted\n", ps.ByName("tokenId"))
-		// If minted, serve json from MetadataDir
-	} else {
-		fileBytes, err := os.ReadFile(fmt.Sprintf("%s%s.json", EnvConfigs.MetadataDir, ps.ByName("tokenId")))
-		if err != nil {
-			fmt.Fprintf(w, "Metadata For Token: %s Not Found\n", ps.ByName("tokenId"))
-		}
-
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.Write(fileBytes)
+		return
+	}
+	fileBytes, err := os.ReadFile(fmt.Sprintf("%s%s.json", EnvConfigs.MetadataDir, ps.ByName("tokenId")))
+	if err != nil {
+		fmt.Fprintf(w, "Metadata For Token: %s Not Found\n", ps.ByName("tokenId"))
 	}
 
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Write(fileBytes)
 }
 
 // Use infura to check if token has been minted using contracts onlyOwner() function.
@@ -59,9 +66,11 @@ func Minted(c *ethclient.Client, token int64) bool {
 func main() {
 	InitEnvConfigs()
 
+	w := NewWeb3(EnvConfigs.InfuraRPC)
+
 	router := httprouter.New()
 	router.GET("/", Index)
-	router.GET("/metadata/:tokenId", Metadata)
+	router.GET("/metadata/:tokenId", w.Metadata)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
