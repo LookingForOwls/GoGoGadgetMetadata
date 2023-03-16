eae08e25c2305d74414e5918f7cfffcae9b58f27
LookingForOwls <lfo@shuffledao.com>
2023-03-16 12:52:16 -0400

    update token minted query  method

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.20
 require (
 	github.com/ethereum/go-ethereum v1.11.4
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/spf13/viper v1.15.0
+	golang.org/x/time v0.3.0
 )
 
 require (
@@ -17,24 +17,13 @@ require (
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
-	github.com/spf13/afero v1.9.5 // indirect
-	github.com/spf13/cast v1.5.0 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	github.com/tklauser/go-sysconf v0.3.11 // indirect
 	github.com/tklauser/numcpus v0.6.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
-	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/big"
 	"net/http"
 	"os"
 	"strconv"
-	"sync"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 
@@ -17,31 +16,67 @@ import (
 	"golang.org/x/time/rate"
 )
 
-// Rate limit requests 10 per second
-var limiter = rate.NewLimiter(10, 3)
-
 type web3 struct {
 	client *ethclient.Client
 }
 
-// Map for token minted status
-var mintedStatus sync.Map
+// Rate limit requests 10 per second
+var limiter = rate.NewLimiter(20, 3)
+
+// Total number of minted tokens
+var totalSupply int64
+
+func main() {
+	port := os.Getenv("PORT")
+	infura := os.Getenv("INFURA")
+	address := os.Getenv("CONTRACT_ADDRESS")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	if infura == "" {
+		log.Fatal("$INFURA must be set")
+	}
+
+	if address == "" {
+		log.Fatal("$CONTRACT_ADDRESS must be set")
+	}
+
+	if os.Getenv("CHECK_MINTED") == "" {
+		log.Fatal("$CHECK_MINTED must be set")
+	}
+
+	// Create web3 client
+	w, _ := newWeb3(infura)
+
+	// instantiate contract
+	instance, err := NewStorage(common.HexToAddress(address), w.client)
+	if err != nil {
+		log.Fatalf("Failed to instantiate contract: %v", err)
+	}
+
+	// Update total token supply every second
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	go updateTotalSupplyTicker(ticker, instance)
+
+	// Start httprouter
+	router := httprouter.New()
+	router.GET("/", indexHandler)
+	router.GET("/metadata/:tokenId", metadataHandler)
+
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Metadata API!\n")
 }
 
-func newWeb3(rpc string) (*web3, error) {
-	c, err := ethclient.Dial(rpc)
-	if err != nil {
-		return nil, err
-	}
-	return &web3{
-		client: c,
-	}, nil
-}
-
-func (c *web3) metadataHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func metadataHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	checkMinted, _ := strconv.ParseBool(os.Getenv("CHECK_MINTED"))
+	// Set default cache control for failures
+	w.Header().Set("Cache-Control", "public, no-cache")
 	// Convert tokenId in URL to Int64
 	tokenID, err := strconv.ParseInt(ps.ByName("tokenId"), 0, 64)
 	if err != nil {
@@ -49,34 +84,15 @@ func (c *web3) metadataHandler(w http.ResponseWriter, r *http.Request, ps httpro
 		return
 	}
 
-	// Set default cache control for failures
-	w.Header().Set("Cache-Control", "public, no-cache")
-
-	// Check if token is minted if enabled
-	checkowner, _ := strconv.ParseBool(os.Getenv("CHECK_OWNER"))
-
-	// Check if token in mapping
-	_, ok := mintedStatus.Load(tokenID)
-
-	if ok {
+	// If tokenId < totalSupply() return metadata
+	if !checkMinted || tokenID <= totalSupply {
 		serveMetadata(w, tokenID)
 		return
 	}
 
-	// Rate limit infura requests
-	if limiter.Allow() == false {
-		http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
-		return
-	}
-
-	if checkowner && !isMinted(c.client, tokenID) {
-		http.Error(w, fmt.Sprintf("Token %d Not Minted\n", tokenID), http.StatusNotFound)
-		return
-	}
-
-	// Set token as minted in the map
-	mintedStatus.Store(tokenID, true)
-	serveMetadata(w, tokenID)
+	// return error if not minted
+	http.Error(w, fmt.Sprintf("Token %d Not Minted\n", tokenID), http.StatusNotFound)
+	return
 }
 
 func serveMetadata(w http.ResponseWriter, tokenID int64) {
@@ -94,48 +110,28 @@ func serveMetadata(w http.ResponseWriter, tokenID int64) {
 	return
 }
 
-// Use infura to check if token has been minted using contracts onlyOwner() function.
-func isMinted(c *ethclient.Client, tokenID int64) bool {
-	contractAddress := getContractAddress()
-	instance, err := NewStorage(contractAddress, c)
+// Create new Web3 RPC Client
+func newWeb3(rpc string) (*web3, error) {
+	c, err := ethclient.Dial(rpc)
 	if err != nil {
-		log.Printf("Failed to instantiate contract: %v", err)
-		return false
+		return nil, err
 	}
-
-	owner, err := instance.OwnerOf(&bind.CallOpts{}, big.NewInt(tokenID))
-	if err != nil {
-		log.Printf("Infura Error: %v", err)
-	}
-
-	return owner != common.HexToAddress("0x0000000000000000000000000000000000000000")
+	return &web3{
+		client: c,
+	}, nil
 }
 
-func getContractAddress() common.Address {
-	address := os.Getenv("CONTRACT_ADDRESS")
-	if address == "" {
-		log.Fatal("$CONTRACT_ADDRESS must be set")
+// Query contract for totalSupply() and update global variable
+func updateTotalSupplyTicker(ticker *time.Ticker, i *Storage) {
+	for {
+		select {
+		case <-ticker.C:
+			supply, err := i.TotalSupply(&bind.CallOpts{})
+			if err != nil {
+				log.Printf("RPC Error: %v", err)
+				continue // Continue with the next iteration of the loop if there is an error
+			}
+			totalSupply = supply.Int64()
+		}
 	}
-	return common.HexToAddress(address)
-}
-
-func main() {
-	port := os.Getenv("PORT")
-	infura := os.Getenv("INFURA")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-
-	if infura == "" {
-		log.Fatal("$INFURA must be set")
-	}
-
-	w, _ := newWeb3(infura)
-
-	router := httprouter.New()
-	router.GET("/", indexHandler)
-	router.GET("/metadata/:tokenId", w.metadataHandler)
-
-	log.Fatal(http.ListenAndServe(":"+port, router))
 }
